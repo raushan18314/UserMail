90bc8d2b75def038e251aa2d582ff4d0d197916f
Raushan Kumar <102571744+raushan18314@users.noreply.github.com>
2023-02-02 18:05:46 +0530

    Update MailService.java

--- UserMailApp/src/main/java/com/masai/serviceInerface/MailService.java
+++ UserMailApp/src/main/java/com/masai/serviceInerface/MailService.java
@@ -7,7 +7,9 @@ import com.masai.model.Mail;
 public interface MailService {
 
 	public Mail sendMail (Mail mail)throws MailException,UserException;
+	
 	public String StarMail(Integer mailId)throws MailException,UserException;
+	
 	public String deleteMail(Integer mailId)throws MailException,UserException;
 }
 
